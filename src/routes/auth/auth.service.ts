3e47f9507fccdb98d34597e1ebe8b6234ef442b7
minhductran042@gmail.com <minhductran042@gmail.com>
2025-08-10 14:23:50 +0700

    feat: Tạo đăng ký người dùng đơn giản với hash password bằng bycrypt

---
+++ src/routes/auth/auth.service.ts
@@ -0,0 +1,33 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../../shared/services/prisma.service';
+import { HashingService } from '../../shared/services/hashing.service';
+import { Prisma } from '../../../generated/prisma';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        private readonly prismaService: PrismaService,
+        private readonly hashingService: HashingService
+    ) {}
+
+    async register(body: any) {
+        try {
+            const hashedPassword = await this.hashingService.hash(body.password);
+            const user = await this.prismaService.user.create({
+                data: {
+                    email: body.email,
+                    password: hashedPassword,
+                    name: body.name
+                }
+            })
+            return user
+        } catch (error) {
+           if(error instanceof Prisma.PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ConflictException('Email already exists');
+                }
+            }
+            throw error; // rethrow other errors
+        }
+    }
+}
